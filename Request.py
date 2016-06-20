dd62d99b13fd3b30a8240bb95aef4aaea0501053
Zachary Zhao <dukezmz1999@gmail.com>
2016-06-20 19:43:12 -0400

    Added content to Request.py

--- Request.py
+++ Request.py
@@ -1,1 +1,6 @@
+from googlemaps import GoogleMaps
 
+gmaps = GoogleMaps(AIzaSyDK9zd0m8qvDcS-Nl9SxUflRk9nRFi-EuM)
+address = 'Constitution Ave NW & 10th St NW, Washington, DC'
+lat, lng = gmaps.address_to_latlng(address)
+print lat, lng
