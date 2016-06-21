ed3cbcef7ba197ebc2d682761cbb719ecd861e0a
Zachary Zhao <dukezmz1999@gmail.com>
2016-06-21 17:28:20 -0400

    "Edits"

--- Geocoding2.py
+++ Geocoding2.py
@@ -1,9 +1,10 @@
 from googlemaps import Client as GoogleMaps
-from googlemaps.geocoding import geocode
+from googlemaps import geocoding
+from googlemaps import convert
 
 API_Key="AIzaSyDVYcV4L2bOFuiEvjgvtTWsVieX7nZfzeo"
 
 gmaps = GoogleMaps(API_Key)
-address = 'Constitution Ave NW & 10th St NW, Washington, DC'
-lat, lng = gmaps.address_to_latlng
-print lat, lng
+address = raw_input("Enter a location: ")
+coordinates = gmaps.geocode(address)
+print coordinates
--- ISS-Location.py
+++ ISS-Location.py
@@ -1,1 +1,11 @@
+import urllib2
+import json
 
+req = urllib2.Request("http://api.open-notify.org/iss-now.json")
+response = urllib2.urlopen(req)
+
+obj = json.loads(response.read())
+
+print "Success"
+print obj['timestamp']
+print obj['iss_position']['latitude'], obj['iss_position']['latitude']
